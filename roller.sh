c503b7035888001dd729745611815ea274c9b027
Becquerel <becquerel.v.jones@gmail.com>
2020-12-10 02:34:29 -0500

    Add advantage flag; elaborate header message; add modifier and advantage to verbose printout.

--- roller.sh
+++ roller.sh
@@ -5,27 +5,30 @@ PLU=0		# Flat modifier
 REP=1		# Repetitions
 VER=false	# Verbose
 CRI=false	# Crits
+ADV=false	# Advantage
 
 # Print usage
 usage() {
-	echo "Usage: ./roller.sh [-n Number] [-d Die] [-p Modifier] [-r Repetitions] [-cv]"
+	echo "Usage: ./roller.sh [-n Number] [-d Die] [-p Modifier] [-r Repetitions] [-vca]"
 	echo " -n: Number of dice"
 	echo " -d: Faces per die"
 	echo " -p: Flat modifier"
 	echo " -r: Repetitions"
-	echo " -c: Criticals"
-	echo " -v: Verbose"
+	echo " -v: Verbose; Print individual rolls"
+	echo " -c: Criticals; Call out min and max rolls"
+	echo " -a: Advantage; deduct lowest roll of each set"
 }
 
 # Process args
-while getopts 'n:d:p:r:cv' opt; do
+while getopts 'n:d:p:r:vca' opt; do
 	case $opt in
 		n) NUM=$OPTARG ;;
 		d) DIE=$OPTARG ;;
 		p) PLU=$OPTARG ;;
 		r) REP=$OPTARG ;;
-		c) CRI=true ;;
 		v) VER=true ;;
+		c) CRI=true ;;
+		a) ADV=true ;;
 		:) usage; exit 1 ;;
 		?) usage; exit 1 ;;
 	esac
@@ -34,7 +37,10 @@ done
 # Print back what should be rolled
 ROLLREPORT="Rolling: $NUM""d$DIE"
 if [ $PLU -ne 0 ]; then ROLLREPORT="$ROLLREPORT+$PLU"; fi
-if [ $REP -ne 1 ]; then ROLLREPORT="$ROLLREPORT x$REP"; fi
+if [ $REP -ne 1 ]; then ROLLREPORT="$ROLLREPORT, x$REP"; fi
+if [ $VER = true ]; then ROLLREPORT="$ROLLREPORT, verbose"; fi
+if [ $CRI = true ]; then ROLLREPORT="$ROLLREPORT, with crits"; fi
+if [ $ADV = true ]; then ROLLREPORT="$ROLLREPORT, with advantage"; fi
 echo "$ROLLREPORT"
 
 # Roll the dice
@@ -48,6 +54,7 @@ for i in $(seq 1 $REP); do
 
 	# Reset total for new repetition; iterate across die rolls
 	TOTAL=0
+	LOWEST=$DIE
 	for j in $(seq 1 $NUM); do
 
 		# Get a random number from /dev/random
@@ -60,7 +67,11 @@ for i in $(seq 1 $REP); do
 
 		# Constrain random value to die range
 		ROLL="$(( $(( $ROLL % $DIE )) + 1 ))"
-		#ROLL="$(( $ROLL + 1 ))"
+
+		# Record if lowest
+		if [ $ADV = true ] && [ $ROLL -lt $LOWEST ]; then
+			LOWEST=$ROLL
+		fi
 
 		# Find out if crit (-1 failure, 1 success)
 		CRIT=0
@@ -95,8 +106,20 @@ for i in $(seq 1 $REP); do
 		TOTAL="$(( $TOTAL + $ROLL ))"
 	done
 
+	# Subtract lowest roll if advantage
+	if [ $ADV = true ]; then
+		if [ $VER = true ]; then
+			echo "|-Adv.: -$LOWEST"
+		fi
+		TOTAL="$(( $TOTAL - $LOWEST ))"
+	fi
+
 	# Add modifier; print result
+	if [ $VER = true ]; then
+		echo "|-Mod.: +$PLU"
+	fi
 	TOTAL="$(( $TOTAL + $PLU ))"
+
 	if [ $REP -ne 1 ]; then
 		echo "\\-Result $i: $TOTAL"
 	else
